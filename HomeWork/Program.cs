827a108718d85fd47eaf876c4b60450123b81813
Dariy_Ershova <d-r-2015@mail.ru>
2023-01-29 21:53:39 +0500

    1

---
+++ HomeWork/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+string[] array = AskArray();
+string[] result = FindLessThan(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
